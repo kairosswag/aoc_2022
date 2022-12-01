1ddfba81f5a4da36ddf4ec8be55bc51a08e66a47
Kai Roßwag <mail@kair.dev>
2022-12-01 19:48:06 +0100

    Improve

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 use std::fs;
 use std::str::FromStr;
+use std::time::Instant;
 
 fn main() {
+    let now = Instant::now();
     day01();
+    println!("Took {} µs", now.elapsed().as_micros());
 }
 
 pub fn day01() {
-    let res_p1 = fs::read_to_string("input/day01")
-        .expect("Could not open file.")
-        .split("\n\n")
-        .map(|group| {
-            group
-                .split("\n")
-                .map(|val| u32::from_str(val).unwrap_or(0))
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-
-    println!("Day 01 Solution Part 1: {}", res_p1);
-
-    let res_p2 = fs::read_to_string("input/day01")
+    let res = fs::read_to_string("input/day01")
         .expect("Could not open file.")
         .split("\n\n")
         .map(|group| {
@@ -33,9 +22,8 @@ pub fn day01() {
             let mut res = [a, b, c, val];
             res.sort();
             [res[1], res[2], res[3]]
-        })
-        .iter()
-        .sum::<u32>();
+        });
 
-    println!("Day 01 Solution Part 2: {}", res_p2);
+    println!("Day 01 Solution Part 1: {}", res[2]);
+    println!("Day 01 Solution Part 2: {}", res[0] + res[1] + res[2]);
 }

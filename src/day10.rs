54feff2eac75bb5a93279076899cae0ede5226a7
Kai Roßwag <mail@kair.dev>
2022-12-10 14:19:00 +0100

    Solve day 10. Indices are fun./a.out < ~/dev/aoc_2022/input/day05

--- src/day10.rs
+++ src/day10.rs
@@ -14,16 +14,13 @@ enum Operation {
 }
 
 pub fn run() {
-    part1();
-}
-
-pub fn part1() {
     let f = File::open("input/day10").expect("could not open file");
     let reader = BufReader::new(f);
 
     let mut x = 1;
     let mut cycle = 0;
     let mut signal_strength = 0;
+    let mut crt = Vec::with_capacity(240);
 
     for op in reader.lines().map(|l| {
         l.unwrap()
@@ -31,21 +28,43 @@ pub fn part1() {
             .expect("could not parse as Operation")
     }) {
         match op {
-            Operation::Noop => increase_cycle(&mut cycle, &mut signal_strength, x),
+            Operation::Noop => increase_cycle(&mut cycle, &mut signal_strength, x, &mut crt),
             Operation::Add(val) => {
-                increase_cycle(&mut cycle, &mut signal_strength, x);
-                increase_cycle(&mut cycle, &mut signal_strength, x);
+                increase_cycle(&mut cycle, &mut signal_strength, x, &mut crt);
+                increase_cycle(&mut cycle, &mut signal_strength, x, &mut crt);
                 x += val;
             }
         }
     }
 
-    println!("Day 10 Solution Part 1: {}", signal_strength);
+    println!(
+        "Day 10 Solution Part 1: {} in {cycle} steps",
+        signal_strength
+    );
+    print_crt(crt);
 }
 
-fn increase_cycle(cycle: &mut i32, signal_strength: &mut i32, x: i32) {
+fn increase_cycle(cycle: &mut i32, signal_strength: &mut i32, x: i32, crt: &mut Vec<&str>) {
     *cycle += 1;
     if *cycle == 20 || *cycle % 40 == 20 {
         *signal_strength += *cycle * x;
     }
+
+    let row_pos = (*cycle-1)  % 40;
+    if row_pos == x - 1 || row_pos == x || row_pos == x + 1 {
+        crt.push("⬜");
+    } else {
+        crt.push("⬛");
+    }
+}
+
+fn print_crt(crt: Vec<&str>) {
+    print!("\nCRT:\n");
+    for i in 1..=crt.len() {
+        print!("{}", crt[i - 1]);
+        if i % 40 == 0 {
+            print!("\n");
+        }
+    }
+    print!("\n");
 }
